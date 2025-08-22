616e94f22e97fac25e04940acc12e57bfb1b2420
Richard Taylor <108982253+richietaylor@users.noreply.github.com>
2025-08-22 17:31:24 +0200

    [0.0.1] Initialization: Added skelly files

---
+++ README.md
@@ -0,0 +1,23 @@
+# NOSIBLE MPC Server
+
+MCP server for the NOSIBLE Search API
+
+Usage with Claude Desktop:
+
+Example with my dir path, replace with your own: 
+
+```
+{
+  "mcpServers": {
+    "nosible-mcp": {
+      "command": "uv",
+      "args": [
+        "--directory",
+        "C:\Users\RichardTaylor\Desktop\MCP\nosible-mcp", 
+        "run",
+        "main.py"
+      ]
+    }
+  }
+}
+```
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from nosible-mcp!")
+import logging
+import os
+
+from mcp.server.fastmcp import FastMCP
+from nosible import Nosible, ResultSet
+
+# Initialize FastMCP server
+mcp = FastMCP("weather")
+
+
+@mcp.tool()
+def search(query: str) -> ResultSet:
+    """Make a search on the Nosible Search engine."""
+    api_key = os.getenv("NOSIBLE_API_KEY")
+    logging.info(("API key: ", api_key))
+    with Nosible(nosible_api_key=api_key) as nos:
+        results = nos.fast_search(question=query, n_results=10)
+
+    return results
 
 
 if __name__ == "__main__":
-    main()
+    # Initialize and run the server
+    mcp.run(transport='stdio')
